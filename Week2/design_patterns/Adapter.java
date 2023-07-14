93cbdedffab6350e6a19442efe5cfc188ad8dfb0
tranky217 <trantrongky217@gmail.com>
2023-07-14 23:22:19 +0700

    update design pattern signleton

---
+++ Week2/design_patterns/Adapter.java
@@ -0,0 +1,4 @@
+package Week2.design_patterns;
+
+public class Adapter {
+}
---
+++ Week2/design_patterns/Singleton.java
@@ -0,0 +1,27 @@
+package Week2.design_patterns;
+
+public class Singleton {
+    private static volatile Singleton instance;
+    // volatile for blocking accessing resource while it is partially constructed
+    private String database;
+
+    // ensure that singleton class can only be access via getInstance method
+    private Singleton(String database){
+        this.database = database;
+    }
+
+    public static Singleton getInstance(String database){
+        // avoid read ref directly from memory -> access by local variable
+        Singleton result = instance;
+        // prevent create different instances from different threads
+        if (result == null){
+            synchronized (Singleton.class){
+                result = instance;
+                if(result == null){
+                    instance = result = new Singleton(database);
+                }
+            }
+        }
+       return result;
+    }
+}
